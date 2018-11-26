e294f60e6fd7cb6ed3f54b8d37af770cc2f0c7fd
Amin Khan <akhan@aacn.org>
2018-11-26 15:42:36 -0800

    ADDED MATERIAL DESIGN AND STYLED BUTTONS

--- src/components/aacn_form/organisms/aacn_form.jsx
+++ src/components/aacn_form/organisms/aacn_form.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
 
 
 class AacnForm extends Component {
@@ -10,20 +12,24 @@ class AacnForm extends Component {
     renderForm(){
         let id=0;
         let tot= [];
-        let num = this.props.num;
-        console.log(num)
-        while(num>0){
-            tot.push(<input type="text" key={id} />)
-           id++
-            num--
-        }
+        let entries= this.props.config.fields
+        console.log(entries.length)
+    tot = entries.map((el,i)=>(<label value={el.label} key={id}>{el.label} <input type={el.type} size={el.size} placeholder={el.placeholder}/></label>) )
         console.log("My totl", tot)
+        if(this.props.config.buttons){
+         for(let i=0;i<this.props.config.buttons.length;i++){
+             console.log("buttons detected")
+             tot.push(<Button variant="contained" color="primary">{this.props.config.buttons[i].name}</Button>)
+         }
+        }
         return tot
     }
     render() { 
         return (
             <>
+            <form >
         {this.renderForm()}
+            </form>
             </>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import AacnForm from './components/aacn_form/organisms/aacn_form.jsx'
 
-function mountForm(){
 
+var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+try { console.log(!!isIE11)}
+catch (err) {console.log("NO SUCH ERROR",err)}
+function mountForm(){
 
 let myNodeList = document.querySelectorAll('div[id^="form"]')
 //using a 'for..in' loop rather than forEach as ie11 does not seem to treat Nodelists as Arrays and cannot use Array.from()
 // Must not use 'for..of' since it breaks ie11
   for(let i =0;i<myNodeList.length;i++){
-   
-    ReactDOM.render(<AacnForm num={parseInt(myNodeList[i].dataset.config)}  />, myNodeList[i])
+   //JSON.parse(myNodeList[i].dataset.config)
+    ReactDOM.render(<AacnForm config={myNodeList[i].dataset.config[0]!='{'?require(`./data/${myNodeList[i].dataset.config}.js`):JSON.parse(myNodeList[i].dataset.config)} />, myNodeList[i])
   }
 
-  myNodeList.forEach(el=>ReactDOM.render(<AacnForm num={parseInt(el.dataset.config)} />,el))
+  myNodeList.forEach(el=>ReactDOM.render(<AacnForm config={el.dataset.config[0]!='{'?require(`./data/${el.dataset.config}.js`):JSON.parse(el.dataset.config)} />,el))
 console.log(window.globalVar)
 //myNodeList.filter((el,i)=>{/^form/.test(el.id)}).forEach(el=>el.innerHTML="form")
 
