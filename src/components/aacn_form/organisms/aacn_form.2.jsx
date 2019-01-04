e8202f8804e05a8cec0b2cd83a57c8a12b5b15ec
Amin Khan <akhan@aacn.org>
2019-01-04 15:51:56 -0800

    CREATE AACN_FORM_2

---
+++ src/components/aacn_form/organisms/aacn_form.2.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import guidGenerator from '../../function_library/guid_generator.js'
+
+ 
+
+
+
+class AacnForm extends Component {
+    constructor(props){
+        super(props)
+        this.state={d:guidGenerator()}
+    }
+
+    /* check for config prop, if present populate the form */
+
+render(){
+
+    return (
+
+    )
+}
+
+}
+
+AacnForm.proptypes = {
+    config:PropTypes.object
+}
+
+export default AacnForm
